c7b2eb178ba41c82d7300e2d02a3a20d704ef1bc
AlejandroDVDSM <bysalexx@gmail.com>
2025-02-23 20:48:28 +0000

    Add characters sprites sheet and create player data scriptaable object

---
+++ Assets/Scripts/ScriptableObjects/CardData/BaseCardData.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "NewBaseCard", menuName = "Card / Base Card Data")]
+    public class BaseCardData : ScriptableObject
+    {
+        public string Name;
+        
+        public Sprite Sprite;
+        
+        public Color BackgroundColor = Color.white;
+        
+        public int MinValue;
+        
+        public int MaxValue;
+        
+        public bool IgnoreData;
+
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/CardData/EnemyCardData.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "NewEnemyCard", menuName = "Card / Enemy")]
+    public class EnemyCardData : BaseCardData
+    {
+        
+    }
+}
